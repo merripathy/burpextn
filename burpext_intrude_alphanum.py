904cc5291648e57afb17a03377233078aad7f534
merripathy <37133383+merripathy@users.noreply.github.com>
2020-03-07 12:49:12 +0530

    Update burpext_intrude_alphanum.py

--- burpext_intrude_alphanum.py
+++ burpext_intrude_alphanum.py
@@ -6,7 +6,7 @@ from burp import IIntruderPayloadGenerator
 #Alphanumeric payload 
 PAYLOADS = [
     'a', 'b', 'c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r',
-     's','t','u','v','w','x','y','z'
+     's','t','u','v','w','x','y','z',0,1,2,3,4,5,6,7,8,9
     ]
 for i in range(10):
     PAYLOADS.append(i)
